e82ec5a860aa472b6262f8896e03c3e6bd3e416b
DESKTOP-DF633JQ\glasvisax <glasvizax@gmail.com>
2026-03-15 16:27:34 +0300

    refactor(logging): refine log macros and delete unnecessary logging test

--- src/Logging.h
+++ src/Logging.h
@@ -7,10 +7,9 @@ extern std::shared_ptr<spdlog::logger> g_stderr_logger;
 
 void initDefaultLoggers();
 
-#define LOG_TRACE(_msg_) SPDLOG_LOGGER_TRACE(g_stdout_logger, _msg_);
-#define LOG_DEBUG(_msg_) SPDLOG_LOGGER_DEBUG(g_stdout_logger, _msg_);
-#define LOG_INFO(_msg_) SPDLOG_LOGGER_INFO(g_stdout_logger, _msg_);
-#define LOG_WARN(_msg_) SPDLOG_LOGGER_WARN(g_stderr_logger, _msg_);
-#define LOG_ERROR(_msg_) SPDLOG_LOGGER_ERROR(g_stderr_logger, _msg_);
-#define LOG_CRITICAL(_msg_) SPDLOG_LOGGER_CRITICAL(g_stderr_logger, _msg_);
-
+#define LOG_TRACE(...) SPDLOG_LOGGER_TRACE(g_stdout_logger, __VA_ARGS__);
+#define LOG_DEBUG(...) SPDLOG_LOGGER_DEBUG(g_stdout_logger, __VA_ARGS__);
+#define LOG_INFO(...) SPDLOG_LOGGER_INFO(g_stdout_logger, __VA_ARGS__);
+#define LOG_WARN(...) SPDLOG_LOGGER_WARN(g_stderr_logger, __VA_ARGS__);
+#define LOG_ERROR(...) SPDLOG_LOGGER_ERROR(g_stderr_logger, __VA_ARGS__);
+#define LOG_CRITICAL(...) SPDLOG_LOGGER_CRITICAL(g_stderr_logger, __VA_ARGS__);
